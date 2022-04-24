be346d59dffe16283075ded1568c9ff469edcb1f
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2022-04-24 11:33:43 +0200

    add: tick and helper fns

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,73 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(s: &str) {
-    let mut msg = String::from("Hello, ");
-    msg.push_str(s);
-    alert(&msg);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn live_neighbors_count(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                println!("delta row: {}, delta col: {}", delta_row, delta_col);
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (col + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
+// JS exported public method
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors_count = self.live_neighbors_count(row, col);
+
+                let next_val = match (cell, live_neighbors_count) {
+                    // Any live cell with fewer than two live neighbours dies, as if caused by underpopulation
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Any live cell with two or three live neighbours lives on to the next generation
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Any live cell with more than three live neighbours dies, as if by overpopulation
+                    (Cell::Alive, x) if x >= 3 => Cell::Dead,
+                    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_val;
+            }
+        }
+
+        self.cells = next;
+    }
 }
